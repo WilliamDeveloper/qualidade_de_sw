a68470a1f14589320f13cba9e31bfcfb1cdcdd63
William.Goulart.Pacheco <william_clarck@hotmail.com>
2016-09-13 21:06:13 -0300

    projeto_inicial

--- test/br/com/calculadora/CalculadoraTest.java
+++ test/br/com/calculadora/CalculadoraTest.java
@@ -13,26 +13,6 @@ import static org.junit.Assert.*;
 
 public class CalculadoraTest {
    
-    /*
-    public CalculadoraTest() {
-    }
-    
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
-    @Before
-    public void setUp() {
-    }
-    
-    @After
-    public void tearDown() {
-    }
-*/
     @Test
     public void testaSeDoisMaisDoisIgualAquatro(){
         Calculadora calculadora = new Calculadora();
